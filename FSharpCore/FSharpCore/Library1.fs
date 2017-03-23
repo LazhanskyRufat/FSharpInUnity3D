19c35043d94b1c429f260fe2d05e85ee0968543c
Rufat Lazhansky <rufat.lazhansky@dtigames.com>
2017-03-23 13:47:51 +0300

    Initial project setup

---
+++ FSharpCore/FSharpCore/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FSharpCore
+
+type Class1() = 
+    member this.X = "F#"
---
+++ FSharpInUnity3D_UnityProject/Assets/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+using UniRx;
+
+using FSharpCore;
+
+
+namespace Test
+{
+    public class Test : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.LogError(new Class1().X);
+        }
+
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
